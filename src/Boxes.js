db64f3e4a9ecd98c3fab58c131c4acbfa18578e1
Iaroslav Uliantcev <doudou239@gmail.com>
2024-09-22 23:22:02 +0300

    Add spawning of the boxes.

--- src/Boxes.js
+++ src/Boxes.js
@@ -48,50 +48,68 @@
 //   </>
 // }
 
-import { useRef, useState } from "react";
-import { Vector3 } from "three";
-import { useFrame } from "@react-three/fiber";
+import {useRef, useState} from "react";
+import {Vector3} from "three";
+import {useFrame} from "@react-three/fiber";
 
-function Box({ color }) {
+function Box({color}) {
   const box = useRef();
+  const time = useRef(0)
   const [xRotSpeed] = useState(() => Math.random());
   const [yRotSpeed] = useState(() => Math.random());
   const [scale] = useState(() => Math.pow(Math.random(), 2.0) * 0.5 + 0.05);
-  const [position, setPosition] = useState(() => resetPosition());
+  const [position, setPosition] = useState(getInitialPosition);
+
+  function getInitialPosition() {
+    let v = new Vector3((Math.random() * 2 - 1) * 3, Math.random() * 2.5 + 0.1, (Math.random() * 2 - 1) * 15);
+    if (v.x < 0) v.x -= 1.75;
+    if (v.x > 0) v.x += 1.75;
+
+    return v
+  }
 
   function resetPosition() {
     let v = new Vector3(
       (Math.random() * 2 - 1) * 3,
       Math.random() * 2.5 + 0.1,
-      (Math.random() * 2 - 1) * 15
+      Math.random() * 10 + 10
     );
     if (v.x < 0) v.x -= 1.75;
     if (v.x > 0) v.x += 1.75;
 
-    return v;
+    setPosition(v)
   }
 
   useFrame((state, delta) => {
-    box.current.position.set(position.x, position.y, position.z);
+    time.current += delta * 1.2
+    const newZ = position.z - (time.current)
+
+    // reset of position
+    if (newZ < -10) {
+      resetPosition()
+      time.current = 0
+    }
+
+    box.current.position.set(position.x, position.y, newZ);
     box.current.rotation.x += delta * xRotSpeed;
     box.current.rotation.y += delta * yRotSpeed;
-  });
+  }, [xRotSpeed, yRotSpeed, position]);
 
   return (
     <mesh ref={box} scale={[scale, scale, scale]} castShadow>
-      <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color={color} envMapIntensity={0.15} />
+      <boxGeometry args={[1, 1, 1]}/>
+      <meshStandardMaterial color={color} envMapIntensity={0.15}/>
     </mesh>
   );
 }
 
 export function Boxes() {
-  const arr = Array.from({ length: 100 }, (_, i) => i); // Создаем массив из 100 элементов
+  const arr = Array.from({length: 100}, (_, i) => i); // Создаем массив из 100 элементов
 
   return (
     <>
       {arr.map((i) => (
-        <Box key={i} color={i % 2 === 0 ? [0.4, 0.1, 0.1] : [0.05, 0.15, 0.4]} />
+        <Box key={i} color={i % 2 === 0 ? [0.4, 0.1, 0.1] : [0.05, 0.15, 0.4]}/>
       ))}
     </>
   );
