0baa4d20b6304ae69695f6ccfcec0c32b59bf1f8
Iaroslav Uliantcev <doudou239@gmail.com>
2024-09-22 22:05:29 +0300

    Add rotating wheels.

--- src/Car.js
+++ src/Car.js
@@ -1,9 +1,12 @@
-import {useLoader} from "@react-three/fiber";
+import {useFrame, useLoader} from "@react-three/fiber";
 import {GLTFLoader} from "three/addons";
 import {useEffect} from "react";
 import {Mesh} from "three";
 
-export function Car(){
+// This work is based on "Chevrolet Corvette (C7)" (https://sketchfab.com/3d-models/chevrolet-corvette-c7-2b509d1bce104224b147c81757f6f43a)
+// by Martin Trafas (https://sketchfab.com/Bexxie) licensed under CC-BY-4.0 (http://creativecommons.org/licenses/by/4.0/)
+
+export function Car() {
   const gltf = useLoader(
     GLTFLoader,
     process.env.PUBLIC_URL + "models/car/scene.gltf"
@@ -13,13 +16,27 @@ export function Car(){
     gltf.scene.scale.set(0.005, 0.005, 0.005);
     gltf.scene.position.set(0, -0.035, 0);
     gltf.scene.traverse((object) => {
-      if (object instanceof Mesh){
+      if (object instanceof Mesh) {
         object.castShadow = true
         object.receiveShadow = true;
         object.material.envMapIntensity = 20;
       }
     })
   }, [gltf])
+
+
+  // rotating wheels
+  useFrame((state) => {
+    const t = state.clock.getElapsedTime();
+
+    const group = gltf.scene.children[0].children[0].children[0];
+    if (group) {
+      group.children[0].rotation.x = t * 2
+      group.children[2].rotation.x = t * 2
+      group.children[4].rotation.x = t * 2
+      group.children[6].rotation.x = t * 2
+    }
+  });
 
   return <primitive object={gltf.scene}/>
 }
